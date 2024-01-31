cf1dca3386774ea5eb9d1d1a58e540e818419867
KimDoYoung <kdy987@gmail.com>
2024-01-31 16:52:33 +0900

    ipo test

--- src/main/java/kr/co/kfs/asseterp/ipo/tracking/Main.java
+++ src/main/java/kr/co/kfs/asseterp/ipo/tracking/Main.java
@@ -1,11 +1,23 @@
 package kr.co.kfs.asseterp.ipo.tracking;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Main {
-    public static void main(String[] args) {
+	
+	private static final String START_URL = "https://www.38.co.kr/html/fund/index.htm?o=r";
+	private static final String FILE_PATH = "C:/Users/deHong/tmp/ipo.txt";
+	
+    public static void main(String[] args) throws IOException {
         final Logger logger = LoggerFactory.getLogger(Main.class);
+        logger.debug("");
         // Step 1: Crawl the website and save data to a text file
         crawlAndSaveToFile();
 
@@ -15,9 +27,15 @@ public class Main {
         System.out.println("Web Crawling and Data Insertion completed successfully!");
     }
 
-    private static void insertDataIntoPostgres() {
-        System.out.println("crawling...");
+    private static void insertDataIntoPostgres() throws IOException {
+        Document doc = Jsoup.connect(START_URL).get();
+        Elements links = doc.select("a[href]");
 
+        try (FileWriter writer = new FileWriter(FILE_PATH)) {
+            for (Element link : links) {
+                writer.write("Link: " + link.attr("href") + "\n");
+            }
+        }
     }
 
     private static void crawlAndSaveToFile() {
