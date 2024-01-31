b56a3100d6f7892852bc2be50fd87f7a2d545811
KimDoYoung <kdy987@gmail.com>
2024-01-31 15:45:41 +0900

    first commit

---
+++ src/main/java/kr/co/kfs/asseterp/ip/tracking/Main.java
@@ -0,0 +1,27 @@
+package kr.co.kfs.asseterp.ip.tracking;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+    public static void main(String[] args) {
+        final Logger logger = LoggerFactory.getLogger(Main.class);
+        // Step 1: Crawl the website and save data to a text file
+        crawlAndSaveToFile();
+
+        // Step 2: Insert data into PostgreSQL
+        insertDataIntoPostgres();
+
+        System.out.println("Web Crawling and Data Insertion completed successfully!");
+    }
+
+    private static void insertDataIntoPostgres() {
+        System.out.println("crawling...");
+
+    }
+
+    private static void crawlAndSaveToFile() {
+        System.out.println("insert db");
+
+    }
+}
